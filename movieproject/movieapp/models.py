d9b42205146e9bcfe049876a5c770ac79da7b0a1
Gangaanju <anjana2004anjana@gmail.com>
2023-11-09 20:17:01 +0530

    added

---
+++ movieproject/movieapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Movie(models.Model):
+    name=models.CharField(max_length=250)
+    director=models.CharField(max_length=250)
+    desc=models.TextField()
+    year=models.IntegerField()
+    img=models.ImageField(upload_to='gallery')
+
+    def __str__(self):
+        return self.name
+
+
